23d83002894f002ec01b5ffd87cdf0d719ce4f3d
chrisorve <chrorn9@gmail.com>
2018-08-29 14:23:51 -0500

    s

--- hive.py
+++ hive.py
@@ -1,10 +1,6 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, HiveContext
 
-
-spark = SparkSession.builder.getOrCreate()
-spark.sparkContext.setLogLevel("ERROR")
-
 SparkContext.setSystemProperty("hive.metastore.uris", "thrift://nn1:9083").setLogLevel("ERROR")
 sparkSession = (SparkSession.builder.appName('hive_connection').enableHiveSupport().getOrCreate())
 hive_command = sparkSession.sql('USE MEDIX')
