a447a9cadb9b3c239f5f4031c8b0b763a99aa626
chrisorve <chrorn9@gmail.com>
2018-08-29 14:24:55 -0500

    c

--- hive.py
+++ hive.py
@@ -1,8 +1,9 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, HiveContext
 
-SparkContext.setSystemProperty("hive.metastore.uris", "thrift://nn1:9083").setLogLevel("ERROR")
+
+sparkContext.setSystemProperty("hive.metastore.uris", "thrift://nn1:9083")
 sparkSession = (SparkSession.builder.appName('hive_connection').enableHiveSupport().getOrCreate())
 hive_command = sparkSession.sql('USE MEDIX')
 hive_command = sparkSession.sql('SELECT COUNT(*) FROM EXT_TABLE_VISITAS')
-
+hive_command.show()
